b523f7106b13c0cb1f57497fea56df8998c1c0c5
Gautamavell <vellgautam@gmail.com>
2025-02-19 15:28:23 +0530

    server connected

--- server/dbs/studentsdb.js
+++ server/dbs/studentsdb.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 
 const ConnectStudentsDB= async () => {
     try{
-        const connection=await mongoose.connect("mongodb://127.0.0.1:27017/students");
+        const connection=await mongoose.connect("mongodb+srv://user_apti:123@apti-ground.jgpcc.mongodb.net/?retryWrites=true&w=majority&appName=Apti-Ground",{dbName:"students"});
         console.log("connected to studentsDB");
         return connection;
         
